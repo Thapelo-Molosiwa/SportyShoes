22d253a7a0056ccf184c3b6fb2243a6c58e3dff7
Isaac Thapelo Molosiwa <123638341+Thapelo-Molosiwa@users.noreply.github.com>
2024-08-01 10:30:54 +0200

    added framework

---
+++ sportyshoes/src/test/java/com/sportyshoes/sportyshoes/SportyshoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportyshoes.sportyshoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportyshoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
